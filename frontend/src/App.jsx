d1644f1964968b1aa5526bcc8a7ddbb9da4d253c
Gustavo Gonzaga <94642849+Gustavo13132@users.noreply.github.com>
2022-04-20 09:20:16 -0300

    Frontend
    Começando a fazer a pagina de inicio que renderizara as perguntas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import "./App.css";
 import Home from "./Components/Home";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/Inicio" element={<Home />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,42 +1,18 @@
-import React, { useState } from "react";
-import axios from 'axios'
-import "./../App.css";
+import React from "react";
+import { BsFileEarmarkPlus } from "react-icons/bs";
 
 function Home() {
-const [email, setEmail] = useState("")
-const [senha, setSenha] = useState("")
-
-function send() {
-    let form = {
-        email: email,
-        password: senha
-    }
-    axios.post("/verifyLogin", form).then(res => {
-        if(res.data === "nouser") {
-            document.querySelector(".err").innerText = "Email ou Senha Incorretos!"
-        } else {
-            
-        }
-    })
-}
-
   return (
-    <div className="flex-home">
-      <div className="img-container">
-        <img src="estudando-home.jpg" className="home-img" alt="" />
+    <>
+      <nav></nav>
+      <div className="container-p">
+        <h2 className="per-title">Pergunta</h2>
+        <button className="btn-res">Responder</button>
       </div>
-      <h1 className="h1-home">Duvida na hora da tarefa ou prova?</h1>
-      <div className="form-home">
-        <div className="flex-form">
-          <h1 className="facaL">Faça o login</h1>
-          <hr />
-          <p className="err"></p>
-          <input type="email" placeholder="Email" className="input" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input type="password" placeholder="Senha" className="input" value={senha} onChange={(e) => setSenha(e.target.value)} />
-          <button className="btn-entrar" onClick={send}>Entrar</button>
-        </div>
+      <div className="ask" title="Fazer uma pergunta">
+        <BsFileEarmarkPlus className="icon-ask" />
       </div>
-    </div>
+    </>
   );
 }
 
