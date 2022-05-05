6037fed474f259065abcd7a4612b77af78b38a22
Gustavo Gonzaga <94642849+Gustavo13132@users.noreply.github.com>
2022-05-04 22:13:21 -0300

    Frontend e Backend
    Frontend: User Screen.
    Backend: DataBase Deployed, add gitIgnore, att infos of DB and starting to deployment.

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,16 @@
 const Sequelize = require("sequelize")
-const sequelize = new Sequelize('Brainly', 'root', '', {
-    host: '127.0.0.1',
-    dialect: 'mysql'
-})
+const sequelize = new Sequelize({
+    database: "anstion",
+    username: "a3gfdtnh5eeh",
+    password: "pscale_pw_rKx-QeC_iP5mIkJOITHKTy8OhFYmE8zmW07tRWG0Xjc",
+    host: "vrl5xi2i8byq.aws-sa-east-1-1.psdb.cloud",
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+  });
 
 module.exports = sequelize
--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import svg from './../inDevelopment.svg'
 
 function Profile() {
   const [login, setLogin] = useState([]);
@@ -19,9 +20,15 @@ function Profile() {
   console.log(per)
   return (
     <>
-      <div>
+    <div className="container">
+      <div className="container-pro">
         <h1>{login.name}</h1>
-        <p>Numero de perguntas feitas: {per.length}</p>
+        <p style={{'color': "white"}}>Numero de perguntas feitas: {per.length}</p>
+      </div>
+      <div className="img-div">
+        <img className="img-d" src={svg} alt="" />
+        <h1 style={{'textAlign': "center"}}>Em breve mais novidades.</h1>
+      </div>
       </div>
     </>
   );
